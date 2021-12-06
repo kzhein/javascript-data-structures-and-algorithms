81fbee2039bab7a5d484e28a62a629164eeb99c3
Kyaw Zin Hein <kyawzinhein6@gmail.com>
2021-12-06 21:19:58 +0630

    Data Structures - Hash Table

---
+++ data-structures/hash-table/hash-table.js
@@ -0,0 +1,92 @@
+// [
+//   [['plum', '#DDA0DD']],
+//   [['salmon', '#FA8072']],
+//   [
+//     ['maroon', '#800000'],
+//     ['yellow', '#FFFF00'],
+//   ],
+//   [['olive', '#808000']],
+//   [['lightcoral', '#F08080']],
+//   [['mediumvioletred', '#C71585']],
+// ]
+
+class HashTable {
+  constructor(size = 53) {
+    this.keyMap = new Array(size);
+  }
+
+  _hash(key) {
+    let total = 0;
+    let WEIRD_PRIME = 31;
+    for (let i = 0; i < Math.min(key.length, 100); i++) {
+      let char = key[i];
+      let value = char.charCodeAt(0) - 96;
+      total = (total * WEIRD_PRIME + value) % this.keyMap.length;
+    }
+    return total;
+  }
+
+  set(key, value) {
+    let index = this._hash(key);
+
+    // if there is nothing in the hashed index number in array
+    if (!this.keyMap[index]) {
+      this.keyMap[index] = [];
+    }
+
+    this.keyMap[index].push([key, value]);
+  }
+
+  get(key) {
+    let index = this._hash(key);
+
+    if (!this.keyMap[index]) return undefined;
+
+    for (let i = 0; i < this.keyMap[index].length; i++) {
+      if (this.keyMap[index][i][0] === key) {
+        return this.keyMap[index][i][1];
+      }
+    }
+  }
+
+  // return keys from the hash table. similar to Object.keys method in JS
+  keys() {
+    let keysArr = [];
+    for (let i = 0; i < this.keyMap.length; i++) {
+      if (this.keyMap[i]) {
+        for (let j = 0; j < this.keyMap[i].length; j++) {
+          // only return unique keys
+          if (!keysArr.includes(this.keyMap[i][j][0])) {
+            keysArr.push(this.keyMap[i][j][0]);
+          }
+        }
+      }
+    }
+    return keysArr;
+  }
+
+  // return values from the hash table. similar to Object.values method in JS
+  values() {
+    let valuesArr = [];
+    for (let i = 0; i < this.keyMap.length; i++) {
+      if (this.keyMap[i]) {
+        for (let j = 0; j < this.keyMap[i].length; j++) {
+          // only return unique values
+          if (!valuesArr.includes(this.keyMap[i][j][1])) {
+            valuesArr.push(this.keyMap[i][j][1]);
+          }
+        }
+      }
+    }
+    return valuesArr;
+  }
+}
+
+let ht = new HashTable(17);
+ht.set('maroon', '#800000');
+ht.set('yellow', '#FFFF00');
+ht.set('olive', '#808000');
+ht.set('salmon', '#FA8072');
+ht.set('lightcoral', '#F08080');
+ht.set('mediumvioletred', '#C71585');
+ht.set('plum', '#DDA0DD');
---
+++ data-structures/hash-table/notes.md
@@ -0,0 +1,19 @@
+# Hash Table
+
+## Hash Table implementations that are built-in to programming languages
+
+`Python` has `Dictionaries`
+
+`JavaScript` has `Objects` and `Maps`
+
+`Java`, `Go`, and `Scala` have `Maps`
+
+`Ruby` has `Hashes`
+
+## Big O of Hash Tables
+
+Insert - O(1)
+
+Deletion - O(1)
+
+Access - O(1)
