42ab8db4831b4952a4943d4e778de7d89bae93a7
kzhein <kyawzinhein6@gmail.com>
2022-04-08 12:58:49 +0630

    Sorting Algorithms - Selection Sort

---
+++ algorithms/sorting/selection-sort.js
@@ -0,0 +1,25 @@
+// Big O
+// Best - O(n2)
+// Average - O(n2)
+// Worst - O(n2)
+function selectionSort(arr) {
+  for (let i = 0; i < arr.length - 1; i++) {
+    let lowest = i;
+
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[lowest]) {
+        lowest = j;
+      }
+    }
+
+    if (i !== lowest) {
+      let temp = arr[i];
+      arr[i] = arr[lowest];
+      arr[lowest] = temp;
+    }
+  }
+
+  return arr;
+}
+
+selectionSort([0, 2, 34, 22, 10, 19, 17]);
